1e2fabb1fda0338a5e88589d85ad79c3399b3205
shourya <Shourya>
2017-10-22 11:17:56 +0530

    key.py changed again

--- key.py
+++ key.py
@@ -3,17 +3,21 @@ import time
 
 letter = []
 duration = []
+end = 'o'
 
 def callb(key):
-    ti1 = time.time() - t
-    ti1 = str(ti1) #converting float value to string
-    ti2 = ti1[0:5] #cutting the seconds ( time ) , without it , it will print like 0.233446546
+	global end;
+    	ti1 = time.time() - t
+    	ti1 = str(ti1) #converting float value to string
+    	ti2 = ti1[0:5] #cutting the seconds ( time ) , without it , it will print like 0.233446546
     #print("The key",key,"Pressed For",ti2,'seconds')
-    letter.append(str(key)[2])
-    duration.append(ti2)
-    print(str(key)[2])
-    print(ti2)
-    return False #stop detecting more key-releases
+    	end = str(key)[2]
+    	if end != 'x':
+    		letter.append(str(key)[2])
+    		duration.append(ti2)
+    		#print(str(key)[2])
+    		#print(ti2)
+    	return False #stop detecting more key-releases
 
 def callb1(key): #what to do on key-press
     return False #stop detecting more key-presses
@@ -24,6 +28,11 @@ while True:
     		listener1.join()
 
 	t = time.time()
+	if end == 'x':
+		break
 
 	with keyboard.Listener(on_release = callb) as listener: #setting code for listening key-release
     		listener.join()
+
+print(letter)
+print(duration)
