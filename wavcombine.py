d345491c0b7dbb865090f68ca9d8f221a0aac43a
SriramB-98 <31282626+SriramB-98@users.noreply.github.com>
2017-10-23 19:35:40 +0530

    Updated version

--- wavcombine.py
+++ wavcombine.py
@@ -1,12 +1,20 @@
-
 from pydub import AudioSegment
 from pydub.playback import play
 
-audio1 = AudioSegment.from_file("Tabla.wav") #your first audio file
-audio2 = AudioSegment.from_file("Violin.wav") #your second audio file
+
+def combine(x, formattype): # x is a list of tuples ( wavname, starttime) sorted according to starttime, should start with 0 
+    first,second = x[0]
+    mixed = AudioSegment.from_file(first)
+    x.remove((first,second))
+    for a,b in x :
+            silence=AudioSegment.silent(duration=b-len(mixed)+1000+len(AudioSegment.from_file(a)))
+            mixed = mixed+silence
+            mixed = mixed.overlay(AudioSegment.from_file(a), position=b)
+            
+        
+    mixed.export("final.wav", format=formattype) #export mixed  audio file
+    play(mixed)
+    return
 
 
-mixed = audio1.overlay(audio2)          #combine , superimpose audio files
-
-mixed.export("mixed.wav", format='wav') #export mixed  audio file
-play(mixed)  
+combine([("./Tabla.wav",0),("./Violin.wav",10000)] ,'wav')
